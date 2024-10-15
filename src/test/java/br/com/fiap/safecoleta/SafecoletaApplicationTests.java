e9242ca8e7cd3e71bc1cc3f8d07531570260664e
AmandaSacchi <amandasacchi@hotmail.com>
2024-10-14 21:17:54 -0300

    unit test

--- src/test/java/br/com/fiap/safecoleta/SafecoletaApplicationTests.java
+++ src/test/java/br/com/fiap/safecoleta/SafecoletaApplicationTests.java
@@ -1,13 +1,23 @@
 package br.com.fiap.safecoleta;
 
+import br.com.fiap.safecoleta.service.AgendamentoColetaService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import br.com.fiap.safecoleta.repository.AgendamentoColetaRepository;
+import br.com.fiap.safecoleta.repository.CaminhaoRepository;
 
 @SpringBootTest
 class SafecoletaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private AgendamentoColetaRepository agendamentoColetaRepository;
+
+    @MockBean
+    private CaminhaoRepository caminhaoRepository;
+
+    @Autowired
+    private AgendamentoColetaService agendamentoColetaService;
 
 }
